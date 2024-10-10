2dba2e21c672df21c1390ce43588a95d4561eb9e
L4a1n <150847641+L4a1n@users.noreply.github.com>
2024-10-10 12:25:27 +0200

    .

--- Parking_lot_rage.java
+++ Parking_lot_rage.java
@@ -7,29 +7,12 @@ import javafx.stage.Stage;
 import javafx.scene.Group; 
 
 public class Parking_lot_rage extends Application{
-
     @Override
     public void start(Stage stage) {
-        int recSize = 50;
-        int recX = 0;
-        int recY = 0;
-        int[][] matrixPlayer = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        Rectangle[][] rectangle = new Rectangle[3][3];
+        
+
+        // Creating the root Group and adding it to a Scene
         Group root = new Group(); 
-        for (Rectangle[] row : rectangle) {
-            recY += 75;
-            for (Rectangle i : row) {
-                recX += 75;
-                i.setX(recX); 
-                i.setY(recY); 
-                i.setWidth(recSize); 
-                i.setHeight(recSize);      
-                root.getChildren().add(i);
-            }
-        }
-         
-        //Creating a Group object  
-        //Creating a scene object 
         Scene scene = new Scene(root, 600, 300);  
       
         //Setting title to the Stage 
