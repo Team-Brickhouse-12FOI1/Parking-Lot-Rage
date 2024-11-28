8ec6f7bfae5f7d5ba0852ef49a3716419778ac2a
L4a1n <150847641+L4a1n@users.noreply.github.com>
2024-11-28 20:47:26 +0100

    testing gridlayout

--- src/main/java/com/brickhouse/parkinglotrage/MainWindow.java
+++ src/main/java/com/brickhouse/parkinglotrage/MainWindow.java
@@ -1,13 +1,45 @@
 package com.brickhouse.parkinglotrage;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 
 
 public class MainWindow {
-    @FXML
-    private Label text;
 
-@FXML
-protected void onHelloButtonClick() {text.setText("It Works!!!");}
+    @FXML
+    private Rectangle box1;
+    @FXML
+    private Rectangle box2;
+    @FXML
+    private Rectangle box3;
+    @FXML
+    private Rectangle box4;
+    @FXML
+    private Rectangle box5;
+    @FXML
+    private Rectangle box6;
+    @FXML
+    private Rectangle box7;
+    @FXML
+    private Rectangle box8;
+    @FXML
+    private Rectangle box9;
+    @FXML
+    private Rectangle box10;
+    @FXML
+    private Rectangle box11;
+    @FXML
+    private Rectangle box12;
+    @FXML
+    private Rectangle box13;
+    @FXML
+    private Rectangle box14;
+    @FXML
+    private Rectangle box15;
+    @FXML
+    private Rectangle box16;
+    @FXML
+    protected void onBoxEnter() {box1.setFill(Color.rgb(17, 0, 255));}
 }
