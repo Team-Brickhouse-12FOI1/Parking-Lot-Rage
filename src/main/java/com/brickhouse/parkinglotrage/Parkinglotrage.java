16669f64e0cead25ededfdd1e69ab5410843eb7c
L4a1n <150847641+L4a1n@users.noreply.github.com>
2024-11-28 12:38:37 +0100

    .

--- src/main/java/com/brickhouse/parkinglotrage/Parkinglotrage.java
+++ src/main/java/com/brickhouse/parkinglotrage/Parkinglotrage.java
@@ -1,6 +1,8 @@
 package com.brickhouse.parkinglotrage;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Parkinglotrage extends Application {
@@ -9,8 +11,21 @@ public class Parkinglotrage extends Application {
         launch(args);
     }
 
+    private Stage primaryStage;
+    private BorderPane rootLayout;
     @Override
     public void start(Stage primaryStage) {
         System.out.println("Test");
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Parking Lot Rage :)");
+
+    }
+    public void innitRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Parkinglotrage.class.getResource("resources/main-window.fxml"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
